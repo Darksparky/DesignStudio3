810c3189523df625e5f3f155962abb39d8faa66d
Darksparky <Darksparky@users.noreply.github.com>
2021-03-18 19:38:38 -0400

    fixed a bunch of physics issues and camera setup

--- public/Js/computer.js
+++ public/Js/computer.js
@@ -1,3 +1,11 @@
 AFRAME.registerComponent('computer',{
+    init: function(){
+        let CONTEXT_AF = this; 
+        
+        CONTEXT_AF.addEventListener('click', function(){
 
+
+            
+        })
+    }
 })
