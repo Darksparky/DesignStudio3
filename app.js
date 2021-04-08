0ebfe6bb6d7b7d542de9108e53d7e6431e270d80
Darksparky <Darksparky@users.noreply.github.com>
2021-04-08 14:31:34 -0400

    adding socket

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express   = require('express');
 const app       = express();
 const http      = require('http');
 const server    = require('http').createServer(app);  
-
+const io        = require('socket.io')(server);
 
 const LISTEN_PORT = 8080;
 
@@ -14,4 +14,22 @@ console.log("Listening on port: " + LISTEN_PORT );
 //this is call a "route" - basically a url path from your website for static pages
 app.get( '/', function( req, res ){ 
     res.sendFile( __dirname + '/public/MainMenu.html' );
+});
+
+//websockets
+
+io.on('connection', (socket)=>{
+    console.log(socket.id + ' is connected');
+
+    socket.on('disconnect', ()=> {
+        console.log(socket.id + ' is disconnected');
+    });
+
+    //custom events
+
+    socket.on('ClosetOpen', (data) =>{
+        console.log('ClosetOpen event received');
+        io.sockets.emit('closet_open');
+    });
+
 });
