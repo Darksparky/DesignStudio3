aac2a8c3f0f968d175a110ef664d23c3ce43c0f2
Darksparky <Darksparky@users.noreply.github.com>
2021-04-14 20:20:04 -0400

    changed buttons stuff

--- public/Js/mobile-controls.js
+++ public/Js/mobile-controls.js
@@ -22,28 +22,27 @@ AFRAME.registerComponent('mobile-controls', {
     init: function(){
         let camera = document.querySelector('#camera');
         var player = document.querySelector('#playercam');    
-        var direction = new THREE.Vector3();
-        var button = this.data.mobileButton;
+        //var direction = new THREE.Vector3();
+       // var button = this.data.mobileButton;
         var isDown = this.data.isDown;
-        button.addEventListener('touchend', function(){
-            isDown = false;
-        });
-        button.addEventListener('touchstart', function(){
-            isDown = true;
-        // get the cameras world direction
-        });
+        // button.addEventListener('touchend', function(){
+        //     isDown = false;
+        // });
+        // button.addEventListener('touchstart', function(){
+        //     isDown = true;
+        // // get the cameras world direction
+        // });
 
-        button.addEventListener('click', function(){
-            var angle = camera.getAttribute("rotation")
-            var x = 1 * Math.cos(angle.y * Math.PI / 180)
-            var y = 1 * Math.sin(angle.y * Math.PI / 180)
-            var pos = player.getAttribute("position")
-            pos.x -= y;
-            pos.z -= x;
-            player.setAttribute("position", pos)
-        });
-           
-
+        // button.addEventListener('click', function(){
+        //     var angle = camera.getAttribute("rotation")
+        //     var x = 1 * Math.cos(angle.y * Math.PI / 180)
+        //     var y = 1 * Math.sin(angle.y * Math.PI / 180)
+        //     var pos = player.getAttribute("position")
+        //     pos.x -= y;
+        //     pos.z -= x;
+        //     player.setAttribute("position", pos)
+        // });
+      
 
     },
 
